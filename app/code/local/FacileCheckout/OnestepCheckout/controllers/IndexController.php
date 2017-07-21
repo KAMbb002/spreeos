6f6565acb2e1fcf36e5b999ae25ec63fd7b96ea1
Sanjeev Jha <tilkor.software@gmail.com>
2017-07-21 18:20:37 +0530

    Otp

--- app/code/local/FacileCheckout/OnestepCheckout/controllers/IndexController.php
+++ app/code/local/FacileCheckout/OnestepCheckout/controllers/IndexController.php
@@ -717,7 +717,7 @@ class FacileCheckout_OnestepCheckout_IndexController extends Mage_Checkout_Contr
 	
 	public function sendOtpVeryfication($mobile, $otp){
 		$message = urlencode("Your Order OTP is $otp");
-		echo $url ="http://sms.rightchoicesms.com/sendsms/sendsms.php?username=SPREEO&password=ksethi&type=TEXT&sender=Alerts&mobile=$mobile&message=$message";
+		echo $url ="http://sms.rightchoicesms.com/sendsms/sendsms.php?username=RCSPREEO&password=ksethi&type=TEXT&sender=SPREEO&mobile=$mobile&message=$message";
 		$return = file($url);
     }
 	
